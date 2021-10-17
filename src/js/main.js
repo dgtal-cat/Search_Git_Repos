35f0a3962faf4a985ef05456b30f4e5040072ec4
dgtal.cat@gmail.com <d1g1talg1t>
2021-10-17 15:02:29 +0300

    added rendering of search results

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
+const searchResult = document.querySelector('.search-result')
 const searchInput = document.querySelector('.search-input')
-const searchResultBlock = document.querySelector('.search-result-block')
+const searchResultBlock = document.querySelector('.repos-list-block')
+
+const warningPopUp = document.querySelector('.warning-popup')
 
 function debounce(callback, delay) {
     let timeout;
@@ -9,15 +12,41 @@ function debounce(callback, delay) {
     }
 }
 
-async function searchRepos() {
-    const url = 'https://api.github.com/search/repositories?q='
-    return await fetch(url+searchInput.value).then((result) => {
-      if (result.ok) {
-          result.json().then(response => console.log(response))
-      } else {
-
-      }
+function renderResponse(response) {
+    const responseResult = response.items
+    console.log(responseResult)
+    const searchResultList = document.createElement('div')
+    responseResult.forEach((item) => {
+        let searchItem = document.createElement('div')
+        searchItem.classList.add('search-item')
+        searchItem.textContent = item.name
+        searchResultList.appendChild(searchItem)
+        searchResult.innerHTML = ''
+        searchResult.appendChild(searchResultList)
     })
 }
 
-searchInput.addEventListener("keyup", debounce(searchRepos, 1000))
+async function searchRepos() {
+    const url = 'https://api.github.com/search/repositories?q='
+    if (searchInput.value){
+        return await fetch(url+searchInput.value+';per_page=5').then((result) => {
+            if (result.ok) {
+                result.json()
+                    .then(response => {
+                        console.log(response)
+                        renderResponse(response)
+                    })
+                warningPopUp.style.display = 'none'
+            } else {
+                warningPopUp.style.display = 'block'
+            }
+        }).catch((err) => {
+            warningPopUp.style.display = 'block'
+            warningPopUp.textContent = "Search error: " + err
+        })
+    } else {
+        searchResult.innerHTML = ''
+    }
+}
+
+searchInput.addEventListener("keyup", debounce(searchRepos, 500))
